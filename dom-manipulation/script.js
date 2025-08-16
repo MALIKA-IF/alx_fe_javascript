0b49d2afde2423210cd2d80a8657facfa2b8d5f9
Malika Edaif <edmalika957@gmail.com>
2025-08-17 00:10:27 +0100

    create function to export and import data

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -92,3 +92,14 @@ function exportQuotes() {
     URL.revokeObjectURL(url);
 }
 document.getElementById('export').addEventListener('click', exportQuotesToJSON);
+
+function importFromJsonFile(event) {
+    const fileReader = new FileReader();
+    fileReader.onload = function(event) {
+      const importedQuotes = JSON.parse(event.target.result);
+      quotes.push(...importedQuotes);
+      saveQuotes();
+      alert('Quotes imported successfully!');
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
