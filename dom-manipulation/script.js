21b4f59fb203b83ad6c1b0928ccfc23ad76be780
Malika Edaif <edmalika957@gmail.com>
2025-08-16 19:40:15 +0100

    create file index.html and script.js and implement a function that dispaly quotes

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -1,5 +1,5 @@
 
-const quote =[{Text : "quotes 1" , category : 1},
+const quote =[{text : "quotes 1" , category : 1},
     {Text : "quotes 2" , category : 2},
     {Text : "quotes 3" , category : 3}
 
