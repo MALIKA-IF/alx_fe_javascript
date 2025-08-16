7c96829a21eeb2859834ab8a3e5fef3acf796a6b
Malika Edaif <edmalika957@gmail.com>
2025-08-17 00:07:51 +0100

    create function to export data

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -75,3 +75,20 @@ function LocalStorageQotes() {
 function saveQuotesToLocalStorage() {
     localStorage.setItem('quotes', JSON.stringify(quotes));
 }
+
+function exportQuotes() {
+    const jsonStr = JSON.stringify(quotes, null, 2); 
+    const blob = new Blob([jsonStr], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+
+    
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'quotes.json';
+    document.body.appendChild(a);
+    a.click();
+
+    document.body.removeChild(a);
+    URL.revokeObjectURL(url);
+}
+document.getElementById('export').addEventListener('click', exportQuotesToJSON);
